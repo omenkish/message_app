4328a6d95f32c2859db3c1d7047be50fa7fc1ad2
omenkish <omenkish@gmail.com>
2019-08-27 21:52:40 +0100

    Add password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,70 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
+  def new
+  end
+
+  def create
+    user_email = params[:password_reset][:email].downcase
+    if user_email.blank?
+      flash.now[:danger] = "Please provide an email address"
+      return render 'new'
+    end
+
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      return redirect_to root_url
+    end
+    flash.now[:danger] = "Email address not found"
+    render 'new'
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      return render 'edit'
+    end
+    if @user.update_attributes(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      return redirect_to @user
+    end
+    render 'edit'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        flash[:danger] = "Ensure your account is activated. Check the mail we sent to you during registration"
+        redirect_to root_url
+      end
+    end
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset link has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,3 @@ User.create!(name: "Omenkish Kevin",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
-)
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password),
-               activated: true,
-               activated_at: Time.zone.now)
-
-end
